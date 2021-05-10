32e1be7cf2e62a0010d14e64bef5bda606a573a6
kc emmanuel <fidekg123@gmail.com>
2021-05-10 04:55:08 +0100

    arrays

---
+++ src/Alphabet.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Alphabet {
+    public static void main(String[] args) {
+        char[] alpha = new char[26];
+        //97
+        for(int i = 0; i < 26; i++){
+            alpha[i] = (char)(97 + i);
+        };
+        System.out.println(Arrays.toString(alpha));
+    }
+}
